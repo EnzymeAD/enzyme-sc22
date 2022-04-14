8452a76f29ad66d94928524467f3597b027c67b2
Sri Hari Krishna Narayanan <sriharikrishna@gmail.com>
2022-04-14 16:43:08 -0500

    Added a disable hyperthreading script

---
+++ disable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+for i in {64..127}
+do
+   echo “Disabling logical HT core $i.”
+   echo 0 > /sys/devices/system/cpu/cpu${i}/online;
+done
+
+echo "1" | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo
